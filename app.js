77695367b54e1becac59bfd4b6824c3b55d33f2c
Kiverok <kiver566@ukr.net>
2023-08-22 18:56:31 +0300

    midleware

--- app.js
+++ app.js
@@ -1,17 +1,79 @@
-const { request } = require("express");
 const express = require("express");
+const fs = require("fs/promises")
+const moment = require("moment");
+const albums = require("./albums.json");
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("<h2>Home page</h2>")
+
+app.use(async(req, res, next)=> {
+const {method, url} = req;
+const date = moment().format("YYYY-MM-DD_hh:mm:ss");
+await fs.appendFile("./public//server.log", `\n${method} ${url} ${date}`);
+next()
+})
+// app.use((req, res, next)=> {
+//     console.log("First middleware");
+//     next()
+// });
+ 
+app.get("/products", (req, res)=> {
+    res.json([])
 });
 
-app.get("/contacts", (request, response) => {
-    response.send("<h2>Contacts page</h2>")
-}); 
+app.get("/albums", (req, res)=> {
+    res.json(albums)
+});
 
-app.listen(3000, ()=> console.log("server running"))
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const albums = require("./albums")
+
+// const app = express();
+
+// app.set('json spaces', 8)
+
+// app.get("/albums", (req, res)=> {
+
+// res.json(albums)
+// }) 
+
+// app.listen(3000);
+
+
+
+
+
+
+
+
+// const { request } = require("express");
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (request, response) => {
+//     response.send("<h2>Home page</h2>")
+// });
+
+// app.get("/contacts", (request, response) => {
+//     response.send("<h2>Contacts page</h2>")
+// }); 
+
+// app.listen(3000, ()=> console.log("server running"))
 
 
 
